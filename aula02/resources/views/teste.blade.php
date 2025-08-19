8d78a03559e01189347b41c6ef4bfbdf7bd6a130
annabmaker <annabmaker@gmail.com>
2025-08-18 22:22:14 -0300

    Atualizações da aula02

---
+++ aula02/resources/views/teste.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Aqui é a tela de teste!
+    </h1>
+  </body>
+</html>
